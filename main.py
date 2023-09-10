5064fab9c621a588344d8f45e57bdac9885e2a30
THIRUMURUGAN T <thirum667@gmail.com>
2023-09-10 09:07:53 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import folium
+import streamlit as st
+
+# Load Airbnb dataset
+df = pd.read_csv('Airbnb_vis.csv')
+
+# Create a Streamlit web app
+st.title("Airbnb Listing Distribution")
+
+st.title('DataFrame Map Visualization')
+
+# Display the DataFrame
+st.dataframe(df)
+
+# Create a Folium map
+m = folium.Map(location=[df['latitude'].mean(), df['longitude'].mean()], zoom_start=4)
+
+# Add markers to the map
+for _, row in df.iterrows():
+    folium.Marker([row['latitude'], row['longitude']]).add_to(m)
+
+# Display the map using folium_static
+st.write("Map")
+st.write(m)
